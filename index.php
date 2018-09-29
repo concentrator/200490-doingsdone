2f370b007e0a121dce90374cce48a95de5da3407
Dmitry Iovlev <iovlev.d@gmail.com>
2018-09-29 21:30:59 +0300

    Добавляет data.php

--- index.php
+++ index.php
@@ -2,52 +2,7 @@
 
 require_once("functions.php");
 
-$title = "Дела в порядке";
-
-$user = "Константин";
-
-$projects = ["Входящие", "Учеба", "Работа", "Домашние дела", "Авто"];
-
-$tasks = [
-    [
-        'title'    => 'Собеседование в IT компании',
-        'date'     => '01.12.2018',
-        'project'  => 'Работа',
-        'done'     => false
-    ],
-    [
-        'title'    => 'Выполнить тестовое задание',
-        'date'     => '25.12.2018',
-        'project'  => 'Работа',
-        'done'     => false
-    ],
-    [
-        'title'    => 'Сделать задание первого раздела',
-        'date'     => '21.12.2018',
-        'project'  => 'Учеба',
-        'done'     => true
-    ],
-    [
-        'title'    => 'Встреча с другом',
-        'date'     => '22.12.2018',
-        'project'  => 'Входящие',
-        'done'     => false
-    ],
-    [
-        'title'    => 'Купить корм для кота',
-        'date'     => 'нет',
-        'project'  => 'Домашние дела',
-        'done'     => false
-    ],
-    [
-        'title'    => 'Заказать пиццу',
-        'date'     => 'нет',
-        'project'  => 'Домашние дела',
-        'done'     => false
-    ]
-];
-
-// var_dump($tasks);
+require_once("data.php");
 
 $content = include_template("index.php",
         [
