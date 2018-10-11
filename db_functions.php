78efe8b32f50a0b4080208a1e913d0f80c02d8b8
Dmitry Iovlev <iovlev.d@gmail.com>
2018-10-11 15:48:20 +0300

    Добавляет проверку ошибок

--- db_functions.php
+++ db_functions.php
@@ -3,6 +3,7 @@
 function db_get_last_error($link) {
 
     $error = mysqli_error($link);
+    return $error;
 }
 
 function db_get_projects($link, $user_id) {
@@ -13,6 +14,32 @@ function db_get_projects($link, $user_id) {
     if ($result) {
         $projects = mysqli_fetch_all($result, MYSQLI_ASSOC);
         return $projects;
+    } else {
+        return false;
+    }
+}
+
+function db_get_tasks($link, $user_id) {
+
+    $sql = "SELECT id, title, DATE(deadline) as deadline, is_done, project_id FROM task WHERE user_id = $user_id";
+    $result = mysqli_query($link, $sql);
+
+    if ($result) {
+        $tasks = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        return $tasks;
+    } else {
+        return false;
+    }
+}
+
+function db_get_tasks_by_proj($link, $user_id, $proj_id) {
+
+    $sql = "SELECT id, title, DATE(deadline) as deadline, is_done, project_id FROM task WHERE user_id = $user_id and project_id = $proj_id";
+    $result = mysqli_query($link, $sql);
+
+    if ($result) {
+        $tasks = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        return $tasks;
     } else {
         return false;
     }
